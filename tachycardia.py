b6a1223134f1dab7782af61332d095140bac51c7
Chris.Zhou <jz200@duke.edu>
2019-02-04 22:53:32 -0500

    Implemented fuzzy string matching
    Passed all test cases

--- tachycardia.py
+++ tachycardia.py
@@ -1,4 +1,15 @@
 def is_tachycardic(s):
     s = s.lower()
     target = "tachycardic"
-    return target in s
+    if target in s:
+        return True
+    import re
+    letters_only = re.search(r'[a-z]+', s)
+    s = letters_only.group(0)
+    from difflib import SequenceMatcher as SM
+    match_ratio = SM(None, s, target).ratio()
+    number_of_matched_letters = match_ratio * (len(s) + len(target)) / 2
+    print(number_of_matched_letters)
+    return number_of_matched_letters >= (len(target) - 2)
+
+# print(is_tachycardic("tachycard"))
